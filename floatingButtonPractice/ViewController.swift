ffaf706be951127ca26d59902fd1a2ddaaaf41ff
bome24 <81296575+bome24@users.noreply.github.com>
2022-11-01 10:23:23 +0900

    [ADD] #1 - floating stack view

--- floatingButtonPractice/ViewController.swift
+++ floatingButtonPractice/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  @IBOutlet weak var floatingStackView: UIStackView!
+  @IBOutlet weak var floatingButton: UIButton!
+  @IBOutlet weak var firstButton: UIButton!
+  @IBOutlet weak var secondButton: UIButton!
+  @IBOutlet weak var thirdButton: UIButton!
+    
+  lazy var floatingDimView: UIView = {
+    let view = UIView(frame: self.view.frame)
+    view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+    view.alpha = 0
+    view.isHidden = true
+    
+    self.view.insertSubview(view, belowSubview: self.floatingStackView)
+    
+    return view
+  }()
+    
+  lazy var buttonList: [UIButton] =
+  [
+    self.firstButton,
+    self.secondButton,
+    self.thirdButton,
+  ]
+    
+  var isFloating: Bool = false
+    
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func floatingButtonAction(_ sender: Any) {
+        if isFloating {
+            
+        }
+    }
+    
 }
 
